42af8cb0c2278e65c0a8a5bf25b40073d5994e45
Juan Pedro Alcuaz <champietro@users.noreply.github.com>
2021-11-08 19:41:09 -0300

    fdg

--- src/index.ts
+++ src/index.ts
@@ -11,27 +11,27 @@ miFuncion([1, 2, 3],[“a”, “b”, “c”] ) => [1, 'a', 2, 'b', 3, 'c']
 
 */
 
-  let vector1: string[] = new Array(3);
-  let vector2: string[] = new Array(3);
+let vector1: string[] = new Array(3);
+let vector2: string[] = new Array(3);
 
-  vector1[0] = "1";
-  vector1[1] = "2";
-  vector1[2] = "3";
+vector1[0] = "1";
+vector1[1] = "2";
+vector1[2] = "3";
 
-  vector2[0] = "a";
-  vector2[0] = "b";
-  vector2[0] = "c";
- 
-  let funcion = function (vector1:string[] ; vector2[]:string[]) { 
+vector2[0] = "a";
+vector2[0] = "b";
+vector2[0] = "c";
+
+let funcion = function (vector1: string[], vector2: string[]) {
   let vector3: string[] = new Array(6);
   let segundo: number = -1;
-    for (let i = 0; i<6; i++){
-      vector3[i] = vector1[i];
-      segundo = segundo +2;
-      vector3[segundo] = vector2[i];
-    }
-    for (let i = 0; i<6;i++ ){
-      console.log(vector3[i])
-    }
+  for (let i = 0; i < 6; i++) {
+    vector3[i] = vector1[i];
+    segundo = segundo + 2;
+    vector3[segundo] = vector2[i];
+  }
+  for (let i = 0; i < 6; i++) {
+    console.log(vector3[i]);
+  }
 };
-funcion(vector1;vector2);
+funcion(vector1, vector2);
